c95058f499c761cd9efe94cd0a8a973e0920765c
rsmaCD <your_email@example.com>
2017-09-09 14:51:06 +0800

    [rsma]finished Diamond: Given a number n, print a centered diamond.

---
+++ src/main/java/com/twu/introductory_programming_exercises/diamond_exercises/Diamond.java
@@ -0,0 +1,44 @@
+package com.twu.introductory_programming_exercises.diamond_exercises;
+
+import com.utils.Print;
+
+/**
+ * Created by rsma on 09/09/2017.
+ */
+public class Diamond {
+
+    public void printDiamond(int n) {
+        String str = buildStr(n);
+        Print.print(str);
+    }
+//[rsma]finished Diamond: Given a number n, print a centered diamond.
+    private String buildStr(int n) {
+        StringBuffer sb = new StringBuffer();
+        int totalRowNum = n * 2 - 1;
+        for (int i = 0; i < totalRowNum; i++) {
+            if (i <= (totalRowNum - 1) / 2) {
+                for (int j = i; j < (totalRowNum - 1) / 2; j++) {
+                    sb.append(" ");
+                }
+                for (int k = 0; k < 2 * i + 1; k++) {
+                    sb.append("*");
+                }
+                for (int j = i; j < (totalRowNum - 1) / 2; j++) {
+                    sb.append(" ");
+                }
+            } else {
+                for (int j = 0; j < i - (totalRowNum - 1) / 2; j++) {
+                    sb.append(" ");
+                }
+                for (int k = 0; k < 2 * (totalRowNum - i) - 1; k++) {
+                    sb.append("*");
+                }
+                for (int j = 0; j < i - (totalRowNum - 1) / 2; j++) {
+                    sb.append(" ");
+                }
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+}
---
+++ src/test/java/com/twu/introductory_programming_exercises/diamond_exercises/DiamondTest.java
@@ -0,0 +1,58 @@
+package com.twu.introductory_programming_exercises.diamond_exercises;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by rsma on 09/09/2017.
+ */
+public class DiamondTest {
+
+    private Diamond diamond = new Diamond();
+
+    private final ByteArrayOutputStream consoleLog = new ByteArrayOutputStream();
+
+    @Before
+    public void setUp() throws Exception {
+        System.setOut(new PrintStream(consoleLog));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        consoleLog.close();
+    }
+
+    @Test
+    public void should_print_nothing_given_0() throws Exception {
+
+        diamond.printDiamond(0);
+        assertEquals("", consoleLog.toString());
+    }
+
+    @Test
+    public void should_print_one_asterisk_given_1() throws Exception {
+
+        diamond.printDiamond(1);
+        assertEquals("*\n", consoleLog.toString());
+    }
+
+    @Test
+    public void should_print_diamond_asterisk_given_3() throws Exception {
+
+        diamond.printDiamond(3);
+        assertEquals("  *  \n *** \n*****\n *** \n  *  \n", consoleLog.toString());
+    }
+
+    @Test
+    public void should_print_diamond_asterisk_given_2() throws Exception {
+
+        diamond.printDiamond(2);
+        assertEquals(" * \n***\n * \n", consoleLog.toString());
+    }
+}
