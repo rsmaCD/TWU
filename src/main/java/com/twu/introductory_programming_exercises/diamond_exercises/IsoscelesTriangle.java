37f062f439a382e68fbbe57f2df2411a8e7b6274
rsmaCD <your_email@example.com>
2017-09-09 14:32:40 +0800

    [rsma]finished Isosceles Triangle:Given a number n, print a centered triangle.

---
+++ src/main/java/com/twu/introductory_programming_exercises/diamond_exercises/IsoscelesTriangle.java
@@ -0,0 +1,32 @@
+package com.twu.introductory_programming_exercises.diamond_exercises;
+
+/**
+ * Created by rsma on 09/09/2017.
+ */
+public class IsoscelesTriangle {
+
+
+    public void printIsoscelesTriangle(int n) {
+        String str = buildStr(n);
+        print(str);
+    }
+
+    private String buildStr(int n) {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= 2 * n - 1; j++) {
+                if ((i >= -j + n + 1) && (i >= j - n + 1)) {
+                    sb.append("*");
+                } else{
+                    sb.append(" ");
+                }
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    private void print(String str) {
+        System.out.print(str);
+    }
+}
---
+++ src/test/java/com/twu/introductory_programming_exercises/diamond_exercises/IsoscelesTriangleTest.java
@@ -0,0 +1,44 @@
+package com.twu.introductory_programming_exercises.diamond_exercises;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by rsma on 09/09/2017.
+ */
+public class IsoscelesTriangleTest {
+
+    private IsoscelesTriangle isoscelesTriangle = new IsoscelesTriangle();
+
+    private final ByteArrayOutputStream consoleLog = new ByteArrayOutputStream();
+
+    @Before
+    public void setUp() throws Exception {
+        System.setOut(new PrintStream(consoleLog));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        consoleLog.close();
+    }
+
+    @Test
+    public void should_print_one_asterisk_given_1() throws Exception {
+
+        isoscelesTriangle.printIsoscelesTriangle(1);
+        assertEquals("*\n", consoleLog.toString());
+    }
+
+    @Test
+    public void should_print_isosceles_triangle_asterisk_given_3() throws Exception {
+
+        isoscelesTriangle.printIsoscelesTriangle(3);
+        assertEquals("  *  \n *** \n*****\n", consoleLog.toString());
+    }
+}
